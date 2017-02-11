17419ff548aa540097cee0565fdbf0c01897a900
Chris Clifford <cclifford82@gmail.com>
2017-02-10 16:25:22 -0800

    update edit data spec

--- spec/features/edit_product_pages_spec.rb
+++ spec/features/edit_product_pages_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe "the edit a product process" do
   it "edits a  product" do
-    product = Product.create(:name => 'test', :description => 'test', :features => 'test', :part_number => 'test', :cost => 49.99)
-    visit products_path
+    product = Product.create(:name => 'test', :description => 'test', :features => 'test', :part_number => 'test', :cost => 49.99, :origin => "USA", :img_url => "http://www.clipartkid.com/images/226/gummy-bear-lamps-s6DOko-clipart.jpg")
+    visit product_path(product)
     click_link 'Edit'
     fill_in 'Name', :with => 'Gummy Bear'
     click_on 'Add Product'
